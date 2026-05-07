a765a76a9b019b44e8bcad79d6be83bd33184e1f
n-toxic <d48965846@gmail.com>
2026-05-08 02:16:09 +0530

    Update WouterRouter base URL configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Switch, Route, Router as WouterRouter, useLocation, Redirect } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -135,7 +136,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <TooltipProvider>
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <WouterRouter base="">
             <Router />
           </WouterRouter>
           <Toaster />
