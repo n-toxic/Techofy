dd39eedd699af63057cb7b6c265995244fd9a22a
n-toxic <d48965846@gmail.com>
2026-05-08 01:54:12 +0530

    Update users.ts

--- shared/db/src/schema/users.ts
+++ shared/db/src/schema/users.ts
@@ -1,6 +1,6 @@
 import { pgTable, text, serial, timestamp, doublePrecision, pgEnum } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod/v4";
+import { z } from "zod";
 
 export const roleEnum = pgEnum("role", ["USER", "ADMIN"]);
 
