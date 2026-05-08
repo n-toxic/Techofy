03705652663ede91d4589c7996a3c1465e21612f
n-toxic <d48965846@gmail.com>
2026-05-08 17:44:09 +0530

    Add files via upload

---
+++ shared/api-client/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./generated/api.js";
+export * from "./generated/api.schemas.js";
+export { setBaseUrl, setAuthTokenGetter, customFetch } from "./custom-fetch.js";
+export type { AuthTokenGetter, CustomFetchOptions } from "./custom-fetch.js";
