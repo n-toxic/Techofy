d87a9523c305772ea37ca1172da53f0e420fa585
n-toxic <d48965846@gmail.com>
2026-05-08 17:40:49 +0530

    Delete shared directory

--- shared/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./otps";
-export * from "./transactions";
-export * from "./instances";
-export * from "./ports";
-export * from "./support";
-export * from "./server-pool";
